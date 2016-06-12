941d160dea1022983b693767652d10dd7043437f
zhaozq <zhao_zzq2012@163.com>
2016-06-12 17:15:58 +0800

    add framework header file

---
+++ ZQTableChartFramework/ZQTableChartHeader.h
@@ -0,0 +1,17 @@
+//
+//  ZQTableChartHeader.h
+//  ZQTableChartDome
+//
+//  Created by zhaozq on 16/6/12.
+//  Copyright © 2016年 zhaozq. All rights reserved.
+//
+
+
+#ifndef ZQTableChartHeader_h
+#define ZQTableChartHeader_h
+
+#import "ZQTableChartView.h"
+#import "ZQTableChart.h"
+#import "ZQTableCell.h"
+#import "UIView+ZQPlus.h"
+#endif /* ZQTableChartHeader_h */
