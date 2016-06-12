b12ad98fb74f17acb072389ca818158cb7c37c1d
zhaozq <zhao_zzq2012@163.com>
2016-06-12 18:01:13 +0800

    header

---
+++ ZQTableChartFramework/ZQTableChartFramework.h
@@ -0,0 +1,27 @@
+//
+//  ttttttt.h
+//  ttttttt
+//
+//  Created by zhaozq on 16/6/12.
+//  Copyright © 2016年 zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZQTableChartFramework.
+FOUNDATION_EXPORT double ZQTableChartFrameworkVersionNumber;
+
+//! Project version string for ZQTableChartFramework.
+FOUNDATION_EXPORT const unsigned char ZQTableChartFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZQTableChartFramework/PublicHeader.h>
+
+#ifndef ZQTableChartFramework_h
+#define ZQTableChartFramework_h
+
+#import <ZQTableChart/ZQTableChartView.h>
+#import <ZQTableChart/ZQTableChart.h>
+#import <ZQTableChart/ZQTableCell.h>
+#import <ZQTableChart/UIView+ZQPlus.h>
+
+#endif
